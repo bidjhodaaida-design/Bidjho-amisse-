f02720e7764f35847221beb048a453f13807e48c
bidjhodaaida-design <bidjhodaaida@gmail.com>
2026-05-14 20:15:39 +0200

    feat: criar interface premium do app Peso Smart MZ

--- index.tsx
+++ index.tsx
@@ -9,7 +9,7 @@ import App from './App';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+  throw new Error('Could not find root element to mount to');
 }
 
 const root = ReactDOM.createRoot(rootElement);
@@ -17,8 +17,4 @@ root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);W ftebView webView = findViewById(R.id.webview);
-webView.getSettings().setJavaScriptEnabled(true);
-webView.loadUrl("https://seusite.com");WebView webView = findViewById(R.id.webview);
-webView.getSettings().setJavaScriptEnabled(true);
-webView.loadUrl("https://seusite.com"); 
+);
